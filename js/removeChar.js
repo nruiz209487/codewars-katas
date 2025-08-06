5780742110fd60add590fe98251d456d7c592a97
Neos Rafael Ruiz Sanchez <neos.ruiz@iesnervion.es>
2025-08-06 15:12:26 +0200

    removeChar js

---
+++ js/removeChar.js
@@ -0,0 +1,9 @@
+// Copilot: ignore file
+// Kata: RemoveFirstandLastCharacter
+// Level: 8kyu
+// Solved: 2025-08-06
+
+function removeChar(str){
+  let res = str.slice(1,str.length - 1);
+  return res;
+};
