7f5fb1dba2771bc3be093cea2fda681dab84597a
nruiz <neos.ruiz@iesnervion.es>
2025-09-07 11:30:05 +0200

    kata: Create Phone Number.py +  nvim

---
+++ js/Create Phone Number.js
@@ -0,0 +1,8 @@
+// Copilot: ignore file
+// Kata: Create Phone Number
+// Level: 6kyu
+// Solved: 2025-09-07
+
+function createPhoneNumber(n) {
+  return `(${n[0]}${n[1]}${n[2]}) ${n[3]}${n[4]}${n[5]}-${n[6]}${n[7]}${n[8]}${n[9]}`;
+}
---
+++ python/Create Phone Number.py
@@ -0,0 +1,7 @@
+# Copilot: ignore file
+# Kata: Create Phone Number
+# Level: 6kyu
+# Solved: 2025-09-07
+
+def create_phone_number(n):
+    return f"({n[0]}{n[1]}{n[2]}) {n[3]}{n[4]}{n[5]}-{n[6]}{n[7]}{n[8]}{n[9]}"
